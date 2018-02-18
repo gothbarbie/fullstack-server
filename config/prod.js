be05f39918f69599744a208ce4a67492d2c58855
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-18 09:45:22 +0100

    Add MONGO_URI

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: 'mongodb://@ds237808.mlab.com:37808/emaily-dev-gothbarbie',
+  mongoURI: process.env.MONGO_URI,
   mongoUsername: process.env.MONGO_USER,
   mongoPassword: process.env.MONGO_PASS,
   cookieKey: process.env.COOKIE_KEY
