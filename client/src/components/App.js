d715b5db17ccb3bf36544e314d72317ce77a4875
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-18 13:40:09 +0100

    setup placeholder routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,25 @@
 // Rendering
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-const App = () => {
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+const App = ({ children }) => {
   return (
-    <div>Elo</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+      {children}
+    </div>
   )
 }
 
