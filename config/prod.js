3c79436d16baa364692ebec298006c4c720345d6
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-18 09:20:30 +0100

    Change storing of keys

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: 'mongodb://@ds237808.mlab.com:37808/emaily-dev-gothbarbie',
+  mongoUsername: process.env.MONGO_USER,
+  mongoPassword: process.env.MONGO_PASS,
+  cookieKey: process.env.COOKIE_KEY
+}
