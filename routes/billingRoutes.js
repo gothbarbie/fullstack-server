57dc731f3e67b8953b4ae63eaef36ffeb02711f7
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-21 16:08:57 +0100

    Adjust billingRoute to post

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,7 +2,5 @@ const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
 
 module.exports = app => {
-  app.get('/api/billing', (req, res) => {
-    
-  })
+  app.post('/api/billing', (req, res) => {})
 }
