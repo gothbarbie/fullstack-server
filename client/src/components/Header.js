6e4e6747dfe59f50d8a00b0f17f7205ee894306b
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-21 14:31:43 +0100

    Add react-stripe-checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   renderContent() {
@@ -22,7 +23,7 @@ class Header extends Component {
       <nav>
         <div className="container">
           <div className="nav-wrapper">
-            <a href="/" className="left brand-logo">Emaily</a>
+            <Link to={this.props.auth ? '/surveys' : '/' } className="left brand-logo">Emaily</Link>
             <ul id="nav-mobile" className="right">
             {this.renderContent()}
             </ul>
