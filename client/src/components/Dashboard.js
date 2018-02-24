8a15957cf5b10e2c241e6650b55fa0ca0969e5c1
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-24 16:14:03 +0100

    Add materialize icons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => (
   <div>
     <h2>Dashboard</h2>
+    <div className="fixed-action-btn">
+      <Link to="/surveys/new" className="btn-floating btn-large red">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
   </div>
 )
 
