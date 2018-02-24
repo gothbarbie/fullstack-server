cddb65c190ce4fefeb55cd1109889ccfbfd39937
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-24 13:39:56 +0100

    Add sendgrid

--- models/Survey.js
+++ models/Survey.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 const RecipientSchema = require('./Recipient')
 
+
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date
 })
 
 mongoose.model('surveys', surveySchema)
