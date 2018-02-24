e9b3f3928fb1f4ebc9f553243620c5f1b07a6dc0
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-24 16:44:21 +0100

    Start with reduxForm in SurveyForm-component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import SurveyForm from './SurveyForm'
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <section>
+        <h2>New Survey</h2>
+        <SurveyForm />
+      </section>
+    )
+  }
+}
+
+export default SurveyNew
